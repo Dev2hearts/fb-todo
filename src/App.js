2949837f5c54339985401447c21426809f964dd2
dev2hearts <js.park119de@gmail.com>
2023-06-14 11:50:12 +0900

    feat: 목록전체 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import List from "./components/List";
 import Form from "./components/Form";
 
 const App = () => {
-    console.log("App 랜더링");
     // 더미 데이터 state 변수
     const [todoData, setTodoData] = useState([
         { id: 1, title: "할일 1", completed: true },
@@ -12,21 +11,23 @@ const App = () => {
         { id: 3, title: "할일 3", completed: true },
         { id: 4, title: "할일 4", completed: true },
     ]);
+    const handleRemoveClick = () => {
+        setTodoData([]);
+    }
 
     return (
         <div className="flex items-center justify-center w-screen h-screen bg-blue-300">
             <div className="w-4/5 p-6 bg-white rounded-[6px] shadow">
-                <div>
-                    <div className="flex justify-between mb-3">
-                        <h1 className="text-center w-3/4 text-2xl text-cyan-400">
-                            FireBase Todo-List
-                        </h1>
-                    </div>
-                    {/* 할일 목록 */}
-                    <List todoData={todoData} setTodoData={setTodoData} />
-                    {/* 할일 추가 */}
-                    <Form todoData={todoData} setTodoData={setTodoData} />
+                <div className="flex justify-between mb-3">
+                    <h1 className="text-center w-3/4 text-2xl text-cyan-400">
+                        FireBase Todo-List
+                    </h1>
+                    <button className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400 text-[12px]" onClick={handleRemoveClick}>Delete All</button>
                 </div>
+                {/* 할일 목록 */}
+                <List todoData={todoData} setTodoData={setTodoData} />
+                {/* 할일 추가 */}
+                <Form todoData={todoData} setTodoData={setTodoData} />
             </div>
         </div>
     );
