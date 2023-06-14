c6ad44a9959e7cacab6bcb88622d4ec996992a82
dev2hearts <js.park119de@gmail.com>
2023-06-14 12:50:11 +0900

    feat: Todo Title 수정 기능 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
     env: {
-      browser: true,
-      es2021: true,
+        browser: true,
+        es2021: true,
     },
     extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
     overrides: [],
     parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
     plugins: ["react"],
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
     },
-  };
+};
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,13 @@
 import React from "react";
+import { useState } from "react";
+
 const ListItem = ({ item, todoData, setTodoData }) => {
-    console.log("ListItem 렌더링", item);
-    const btnStyle = {
-        color: "#fff",
-        float: " right",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-    };
+    // console.log("ListItem 렌더링", item);
+
+    // 편집 상태 설정 state
+    const [isEdit, setIsEdit] = useState(false);
+    // 편집 상태 타이틀 설정 state
+    const [editTitle, setEditTitle] = useState(item.title);
     const getStyle = _completed => {
         return {
             padding: "10px",
@@ -16,13 +15,32 @@ const ListItem = ({ item, todoData, setTodoData }) => {
         };
     };
     // 이벤트 핸들러
-    const handleClick = _id => {
+    const handleDeleteClick = _id => {
         // 전달된 ID를 검색해서 목록에서 제거
         // 1. 전달된 id로 해당하는 목록 찾기 제외
         // 2. 새로운 목록으로 갱신해서 화면 리랜더링
         // 3. 배열의 고차함수 중 filter 를 사용
         const newTodoData = todoData.filter(item => item.id !== _id);
         setTodoData(newTodoData);
+    };
+    const handleEditClick = () => {
+        setIsEdit(true);
+    };
+    const handleEditChange = e => {
+        setEditTitle(e.target.value);
+    };
+    const handleCancleClick = () => {
+        setIsEdit(false);
+    };
+    const handleSaveClick = _id => {
+        let newTodoData = todoData.map(item => {
+            if (item.id === _id) {
+                item.title = editTitle;
+            }
+            return item;
+        });
+        setTodoData(newTodoData);
+        setIsEdit(false);
     };
     const handleCompletChange = _id => {
         // 중요한 것은 id에 해당하는 것만 수정하면 되는게 아니다.
@@ -36,28 +54,67 @@ const ListItem = ({ item, todoData, setTodoData }) => {
         });
         setTodoData(newTodoData);
     };
-    return (
-        <div className="flex items-center justify-between w-full mb-2 px-4 py-1 text-gray-600 bg-gray-100 border rounded">
-            <div className="items-center" style={getStyle(item.completed)}>
-                {/* defaultChecked : 체크박스에 기본체크 상태 설정 */}
-                <input
-                    type="checkbox"
-                    defaultChecked={item.completed}
-                    onChange={() => handleCompletChange(item.id)}
-                ></input>
-                {item.title}
+    if (isEdit) {
+        // 편집중
+        return (
+            <div className="flex items-center justify-between w-full mb-2 px-4 py-1 text-gray-600 bg-gray-100 border rounded">
+                <div className="items-center w-3/5 px-6">
+                    <input
+                        className="w-full px-3 py-2 mr-3 text-gray-500 rounded"
+                        type="text"
+                        value={editTitle}
+                        onChange={handleEditChange}
+                    />
                 </div>
                 <div className="flex items-center">
-                <button
-                    style={btnStyle}
-                    onClick={() => {
-                        handleClick(item.id);
-                    }}
-                >
-                    X
-                </button>
+                    <button
+                        className="px-4 py-2 float-right"
+                        onClick={() => {
+                            handleSaveClick(item.id);
+                        }}
+                    >
+                        Save
+                    </button>
+                    <button
+                        className="px-4 py-2 float-right"
+                        onClick={handleCancleClick}
+                    >
+                        Cancle
+                    </button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        // 일반상태
+        return (
+            <div className="flex items-center justify-between w-full mb-2 px-4 py-1 text-gray-600 bg-gray-100 border rounded">
+                <div className="items-center flex" style={getStyle(item.completed)}>
+                    {/* defaultChecked : 체크박스에 기본체크 상태 설정 */}
+                    <input
+                        type="checkbox"
+                        defaultChecked={item.completed}
+                        onChange={() => handleCompletChange(item.id)}
+                    ></input>
+                    <span className="ml-3">{item.title}</span>
+                </div>
+                <div className="flex items-center">
+                    <button
+                        className="px-4 py-2 float-right"
+                        onClick={handleEditClick}
+                    >
+                        Edit
+                    </button>
+                    <button
+                        className="px-4 py-2 float-right"
+                        onClick={() => {
+                            handleDeleteClick(item.id);
+                        }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
+        );
+    }
 };
 export default ListItem;
