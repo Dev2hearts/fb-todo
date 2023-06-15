114684a8051138c43189fe38ec0facd29b931b86
dev2hearts <js.park119de@gmail.com>
2023-06-15 18:15:38 +0900

    feat: firebase 연동 SignUp 디버깅 필요

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import SignUpDiv from "../style/UserCss";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+// firebase 연동
+import firebase from "../firebase";
 
 const SignUp = () => {
     const navigate = useNavigate();
@@ -8,9 +10,21 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [pw, setPw] = useState("");
     const [pwConfirm, setPwConfirm] = useState("");
-    const handleSignUp = e => {
+    const handleSignUp = async e => {
         e.preventDefault();
-        // FireBase 에 SignUp 하기
+        try {
+            let createUser = await firebase
+                .auth()
+                .createUserWithEmailAndPassword(email, pw);
+
+            await createUser.user.updateProfile({
+                name: nickName,
+            });
+            console.log("등록된 정보 : ", createUser.user);
+        } catch (err) {
+            console.log(err);
+        }
+        // firebase 에 회원가입 하기
     };
     return (
         <div className="p-6 m-auto mt-5 shadow rounded-md bg-white">
